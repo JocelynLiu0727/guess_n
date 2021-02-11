90ed5dafd285f79b2e34250be213ed7f7fa47c5a
Jocelyn Liu <bettyliu840727@gmail.com>
2021-02-11 11:31:45 +0800

    add count

--- guess_n.py
+++ guess_n.py
@@ -1,6 +1,8 @@
 import random
 r = random.randint(1, 100)
+count = 0 
 while True:
+	count += 1 # count = count + 1
 	number = input('請輸入數字: ')
 	number = int(number)
 	if number == r:
@@ -10,4 +12,5 @@ while True:
 		print('猜錯囉! 比答案大!')
 	else:
 		print('猜錯囉! 比答案小!')
+	print('這是你猜的第', count, '次')
 
